03fb90310685099c6d980dba30e7695807e97225
Doan Duc <dukinfotech@gmail.com>
2023-11-19 11:45:07 +0900

    refactor layout

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,23 +1,11 @@
 "use client";
-import { Inter } from "next/font/google";
-import "../globals.css";
-import { Layout } from "@/components/admin/layout/layout";
-import { Providers } from "../providers";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Layout } from "@/components/admin/layout/layout";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={`${inter.className} dark`}>
-      <body>
-        <Providers>
-          <Layout>{children}</Layout>
-        </Providers>
-      </body>
-    </html>
-  );
+  return <Layout>{children}</Layout>;
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.className} dark`}>
+    <html lang="en" className={inter.className}>
       <body>
         <Providers>{children}</Providers>
       </body>
