cd80136654b356a24d2752788dbee0ef645b13c7
Doan Duc <dukinfotech@gmail.com>
2023-11-25 17:29:49 +0900

    backend: validate accessToken

--- backend/src/modules/accounts/accounts.service.ts
+++ backend/src/modules/accounts/accounts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { Account } from './account.entity';
 
@@ -9,6 +10,12 @@ export class AccountsService {
     @InjectRepository(Account)
     private accountsRepository: Repository<Account>,
   ) { }
+
+  async validateAccessToken(email: string, accessToken: string): Promise<boolean> {
+    const account = await this.accountsRepository.findOneBy({ email });
+    const isMatch = await bcrypt.compare(accessToken, account.hashedAccessToken);
+    return isMatch;
+  }
 
   async findByEmail(email: string): Promise<Account> {
     const account = await this.accountsRepository.findOne({
--- backend/src/modules/auth/strategies/jwt.strategy.ts
+++ backend/src/modules/auth/strategies/jwt.strategy.ts
@@ -3,27 +3,35 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../../users/users.service';
 import { ConfigService } from '@nestjs/config';
+import { AccountsService } from '../../accounts/accounts.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
+    private configService: ConfigService,
     private usersService: UsersService,
-    private configService: ConfigService
+    private accountsService: AccountsService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>("JWT_SECRET"),
+      passReqToCallback: true
     });
   }
 
   // Parse the JWT from incoming requests
-  async validate(payload: any) {
+  async validate(req: Request, payload: any) {
     const { email } = payload;
-    const user = await this.usersService.findByEmail(email);
-    if (!user) {
-      throw new UnauthorizedException();
+    const accessToken = req.headers['authorization'].split(' ')[1];
+
+    const isValid = this.accountsService.validateAccessToken(email, accessToken);
+    if (isValid) {
+      const user = await this.usersService.findByEmail(email);
+      if (user) {
+        return user;
+      }
     }
-    return user;
+    throw new UnauthorizedException();
   }
 }
