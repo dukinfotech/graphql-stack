edb9d71ea6f373f1ae83c36e65b1310907ccba2c
Doan Duc <dukinfotech@gmail.com>
2023-11-25 18:03:59 +0900

    backend: JwtAuthGuard sayHello query

--- backend/src/app.resover.ts
+++ backend/src/app.resover.ts
@@ -1,7 +1,10 @@
+import { UseGuards } from "@nestjs/common";
 import { Query, Resolver } from "@nestjs/graphql";
+import { JwtAuthGuard } from "./modules/auth/guards/jwt-auth.guard";
 
 @Resolver()
 export class AppResolver {
+  @UseGuards(JwtAuthGuard)
   @Query(() => String)
   sayHello(): string {
     return 'Hello World!';
