f40ede381325e7e94b728169051c2cd0cb2ac4ed
Doan Duc <dukinfotech@gmail.com>
2023-12-05 23:59:18 +0700

    hasura: setup roles

--- backend/src/app.resolver.ts
+++ backend/src/app.resolver.ts
@@ -1,10 +1,21 @@
-import { UseGuards } from "@nestjs/common";
-import { Query, Resolver } from "@nestjs/graphql";
-import { CurrentUser, JwtAuthGuard } from "./modules/auth/guards/jwt-auth.guard";
-import { User } from "./modules/users/user.entity";
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import {
+  CurrentUser,
+  JwtAuthGuard,
+} from './modules/auth/guards/jwt-auth.guard';
+import { User } from './modules/users/user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Resolver()
 export class AppResolver {
+  constructor(private readonly configService: ConfigService) {}
+
+  @Query(() => String)
+  showEnv() {
+    const env = this.configService.get<string>('NODE_ENV') || 'development';
+    return `Running environment: ${env}`;
+  }
 
   @UseGuards(JwtAuthGuard)
   @Query(() => String)
