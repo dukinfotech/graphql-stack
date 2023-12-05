c002f33d90b517047465f6619adda7082bbafbdf
Doan Duc <dukinfotech@gmail.com>
2023-12-05 22:59:53 +0700

    hasura: update role names

--- hasura/seeds/postgres/1701786945409_rolesSeed.sql
+++ hasura/seeds/postgres/1701786945409_rolesSeed.sql
@@ -1,5 +1,5 @@
 SET check_function_bodies = false;
 SELECT pg_catalog.setval('public.roles_id_seq', 1, false);
-INSERT INTO public.roles (name) VALUES ('Admin');
-INSERT INTO public.roles (name) VALUES ('Manager');
-INSERT INTO public.roles (name) VALUES ('User');
+INSERT INTO public.roles (name) VALUES ('admin');
+INSERT INTO public.roles (name) VALUES ('manager');
+INSERT INTO public.roles (name) VALUES ('user');
